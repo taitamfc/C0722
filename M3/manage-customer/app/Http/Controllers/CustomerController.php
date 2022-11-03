697a2cc2bdcc3ea2b7ef38f74efa08a14f2382e0
taitamfc <taitam8590@gmail.com>
2022-11-03 11:01:40 +0700

    manage-customer validate request

---
+++ M3/manage-customer/app/Http/Controllers/CustomerController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class CustomerController extends Controller
+{
+    public function index()
+    {
+        $customers = Customer::all();
+        return view('customers.list', compact('customers'));
+    }
+
+    public function create()
+    {
+        return view('customers.create');
+    }
+
+    public function store(Request $request)
+    {   
+        $validated = $request->validate([
+            'name' => 'required|unique:customer|max:6',
+            'email' => 'required',
+            'dob' => 'required'
+        ],
+        [
+            'name.required'=>'truong bat buoc',
+            'name.unique'=>'khong duong trung lap du lieu',
+            'name.max'=>'truong bat buoc be hon :max',
+            'email.required'=>'truong bat buoc',
+            'dob.required'=>'truong bat buoc'
+        ]
+    );
+
+        $customer = new Customer();
+        $customer->name = $request->input('name');
+        $customer->email = $request->input('email');
+        $customer->dob = $request->input('dob');
+        $customer->save();
+
+        //dung session de dua ra thong bao
+        Session::flash('success', 'Tạo mới khách hàng thành công');
+        //tao moi xong quay ve trang danh sach khach hang
+        return redirect()->route('customers.index');
+    }
+
+    public function edit($id)
+    {
+        $customer = Customer::findOrFail($id);
+        return view('customers.edit', compact('customer'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $customer = Customer::findOrFail($id);
+        $customer->name = $request->input('name');
+        $customer->email = $request->input('email');
+        $customer->dob = $request->input('dob');
+        $customer->save();
+
+        //dung session de dua ra thong bao
+        Session::flash('success', 'Cập nhật khách hàng thành công');
+        //cap nhat xong quay ve trang danh sach khach hang
+        return redirect()->route('customers.index');
+    }
+
+    public function destroy($id)
+    {
+        $customer = Customer::findOrFail($id);
+        $customer->delete();
+
+        //dung session de dua ra thong bao
+        Session::flash('success', 'Xóa khách hàng thành công');
+
+        //xoa xong quay ve trang danh sach khach hang
+        return redirect()->route('customers.index');
+    }
+}
